a17e1474e922c2173102dbc7cdaaff1590fd8a13
Todd Page <todd.page@digintent.com>
2018-07-31 17:46:35 -0500

    Works on individual auditions

--- src/inject/inject_full.js
+++ src/inject/inject_full.js
@@ -2,58 +2,49 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 		if (document.readyState === "interactive") {
 			clearInterval(readyStateCheckInterval);
-			console.log('foo');
-			//initPage();
+			setAuditionState();
 		}
 	}, 10);
 });
 
-function initPage() {
-	$('.style6').each(setAuditionState);
-}
+function setAuditionState() {
+	var auditionURL = document.URL;
+	var auditionKey = auditionURL.substring(auditionURL.indexOf('ID=') + 3);
+	var auditionName = document.getElementsByClassName('detailAudSubHead')[0].innerText;
 
-function setAuditionState(i, audition) {
-	if(audition.getElementsByClassName('detailhead').length > 0) {
-		var auditionURL = audition.getElementsByClassName('detailhead')[0].getElementsByTagName('a')[0].href;
-		var auditionKey = auditionURL.substring(auditionURL.indexOf('ID=') + 3);
-		var auditionName = audition.getElementsByClassName('detailbody')[0].innerText;
-
-		chrome.storage.sync.get([auditionKey], function(auditionObject) {
-			if(isEmpty(auditionObject)) {
-				let newAuditionObject = {
-					key: auditionKey,
-					showName: auditionName,
-					new: true,
-					applied: false,
-					dateApplied: '',
-					uninterested: false,
-					dateUninterested: ''
-				}
-
-				chrome.storage.sync.set({[auditionKey]: newAuditionObject});
-
-				initAuditionPosting(audition, newAuditionObject);
-			} else {
-				let auditionData = auditionObject[auditionKey];
-				auditionData.new = false;
-
-				chrome.storage.sync.set({[auditionKey]: auditionData});
-
-				initAuditionPosting(audition, auditionData);
+	chrome.storage.sync.get([auditionKey], function(auditionObject) {
+		if(isEmpty(auditionObject)) {
+			let newAuditionObject = {
+				key: auditionKey,
+				showName: auditionName,
+				new: true,
+				applied: false,
+				dateApplied: '',
+				uninterested: false,
+				dateUninterested: ''
 			}
-		});
 
-	}
+			chrome.storage.sync.set({[auditionKey]: newAuditionObject});
+
+			initAuditionPosting(newAuditionObject);
+		} else {
+			let auditionData = auditionObject[auditionKey];
+			auditionData.new = false;
+
+			chrome.storage.sync.set({[auditionKey]: auditionData});
+
+			initAuditionPosting(auditionData);
+		}
+	});
+
 }
 
-function initAuditionPosting(auditionObj, auditionData) {
+function initAuditionPosting(auditionData) {
 	if(auditionData.uninterested) {
-		var auditionManager = '<div class="audition-manager uninterested clickable toggleable closed ' + auditionData.key + '"><span class="sixteen">&#10006;</span> - '+ auditionData.dateUninterested + ' - ' + auditionData.showName +' </div>';
-		$(auditionObj).addClass('hide');
+		var auditionManager = '<div class="audition-manager uninterested ' + auditionData.key + '"><span class="sixteen">&#10006;</span> - '+ auditionData.dateUninterested + ' - ' + auditionData.showName +' </div>';
 
 	} else if(auditionData.applied) {
-		var auditionManager = '<div class="audition-manager applied clickable toggleable closed ' + auditionData.key + '"><span class="sixteen">&#10004;</span> - '+ auditionData.dateApplied + ' - ' + auditionData.showName +' </div>';
-		$(auditionObj).addClass('hide');
+		var auditionManager = '<div class="audition-manager applied ' + auditionData.key + '"><span class="sixteen">&#10004;</span> - '+ auditionData.dateApplied + ' - ' + auditionData.showName +' </div>';
 
 	} else {
 		var auditionManager = document.createElement('div');
@@ -65,60 +56,44 @@ function initAuditionPosting(auditionObj, auditionData) {
 
 		let markUninterestedInteraction = document.createElement('span');
 		markUninterestedInteraction.className = "pr-10 clickable";
-		markUninterestedInteraction.onclick = function() { markUninterested(auditionObj,auditionData); };
+		markUninterestedInteraction.onclick = function() { markUninterested(auditionData); };
 		$(markUninterestedInteraction).append('<span class="big-x">&#10006;</span> Mark Uninterested</span>');
 
 		$(auditionManager).append(markUninterestedInteraction);
 
 		let markAppliedInteraction = document.createElement('span');
 		markAppliedInteraction.className = "pr-10 clickable";
-		markAppliedInteraction.onclick = function() { markApplied(auditionObj,auditionData); };
+		markAppliedInteraction.onclick = function() { markApplied(auditionData); };
 		$(markAppliedInteraction).append('<span class="check">&#10004;</span> Mark Applied</span>');
 
 		$(auditionManager).append(markAppliedInteraction);
 	}
 
-	$(auditionObj).before(auditionManager);
-	$(auditionObj).addClass('hideable');
-
+	$('.detailAudSubHead').after(auditionManager);
 }
 
-function markUninterested(auditionObj,auditionData) {
+function markUninterested(auditionData) {
 	let today = new Date();
 
 	auditionData.uninterested = true;
 	auditionData.dateUninterested = today.toLocaleDateString('en-us');
 	$('.audition-manager.' + auditionData.key).remove();
-	initAuditionPosting(auditionObj, auditionData);
+	initAuditionPosting(auditionData);
 
 	chrome.storage.sync.set({[auditionData.key]: auditionData});
 }
 
-function markApplied(auditionObj,auditionData) {
+function markApplied(auditionData) {
 	let today = new Date();
 
 	auditionData.applied = true;
 	auditionData.dateApplied = today.toLocaleDateString('en-us');
 	$('.audition-manager.' + auditionData.key).remove();
-	initAuditionPosting(auditionObj, auditionData);
+	initAuditionPosting(auditionData);
 
 	chrome.storage.sync.set({[auditionData.key]: auditionData});
 }
 
-function toggleShowDescription(e) {
-	e.stopImmediatePropagation();
-	if($(e.currentTarget).next().hasClass('hide')) {
-		$(e.currentTarget).removeClass('closed');
-		$(e.currentTarget).addClass('open');
-
-		$(e.currentTarget).next().removeClass('hide');
-	} else {
-		$(e.currentTarget).addClass('closed');
-		$(e.currentTarget).removeClass('open');
-
-		$(e.currentTarget).next().addClass('hide');
-	}
-}
 
 function isEmpty(obj) {
     for(var key in obj) {
